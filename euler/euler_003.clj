7ff689ee92d8169f2cc506f4f002aef7af42f99e
Rickard Dybeck <r.dybeck@gmail.com>
2012-06-30 22:52:43 +0200

    Euler 003 fixup
    better recursion

--- euler/euler_003.clj
+++ euler/euler_003.clj
@@ -2,16 +2,17 @@
 ;
 ; What is the largest prime factor of the number 600851475143 ?
 
-
-(defn factor [n d factors]
-    (if (< n (* d d))
-        (conj factors n)
-        (if (zero? (mod n d))
-            (factor (/ n d) d (conj factors d))
-            (factor n (+ d 1) factors))))
+(defn factor [n-arg d-arg]
+    (loop [n n-arg
+            d d-arg
+            factors []]
+        (if (< n (* d d))
+            (conj factors n)
+            (if (zero? (mod n d))
+                (recur (/ n d) d (conj factors d))
+                (recur n (+ d 1) factors)))))
 
 (defn solve [number]
-    (first (reverse (factor number 2 [])))
-)
+    (first (reverse (factor number 2))))
 
 (prn (solve 600851475143))
